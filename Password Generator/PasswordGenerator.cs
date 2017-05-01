7e3bcb09315feb176c896c5b5a024e8f1bc83ba1
Sukhwinder Singh <sainisukha88@gmail.com>
2017-05-01 19:44:12 +0530

    Password Generator added

---
+++ Password Generator/PasswordGenerator.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Text;
+namespace PasswordGenerator
+{
+    public class Config
+    {
+        public bool alphabets { get; set; }
+        public bool numbers { get; set; }
+        public bool specialCharacters { get; set; }
+        public int length { get; set; }
+
+        public Config()
+        {
+            alphabets = true;
+            numbers = true;
+            specialCharacters = true;
+            length = 10;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = new Config() { length=12 };
+            var password=PasswordGenerator(config);
+            Console.WriteLine(password);
+            Console.ReadLine();
+        }
+
+        public static string PasswordGenerator(Config config)
+        {
+            if (config.length != 0)
+            {
+                const string alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                const string number = "1234567890";
+                const string specialChanracters = "!@#$%^&*()";
+                StringBuilder passwordString = new StringBuilder(String.Empty);
+                StringBuilder generatedPassword = new StringBuilder(String.Empty);
+                try
+                {
+                    if (config.alphabets)
+                    {
+                        passwordString.Append(alphabets);
+                    }
+                    if (config.numbers)
+                    {
+                        passwordString.Append(number);
+                    }
+                    if (config.specialCharacters)
+                    {
+                        passwordString.Append(specialChanracters);
+                    }
+
+                    Random rnd = new Random();
+                    int length = passwordString.Length - 1;
+                    for (int i = 0; i < config.length; i++)
+                    {
+                        int randomNumber = rnd.Next(0, length);
+                        generatedPassword.Append(passwordString[randomNumber]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return generatedPassword.ToString();
+            }
+            throw new Exception("Password Length cannot be zero");
+        }
+    }
+}
